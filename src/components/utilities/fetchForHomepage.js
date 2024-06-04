2bd99614f2c7ca501249ad9539b3a09945722024
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2024-06-04 17:15:15 -0500

    Initial fetch.

--- src/components/utilities/fetchForHomepage.js
+++ src/components/utilities/fetchForHomepage.js
@@ -1,10 +1,11 @@
 // Import
 import React, { useState, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./navbar";
+import Footer from "./footer";
 
 // Initial Fetch
 const FetchForHomepage = () => {
-    // Establish new state;
     const [accounts, setAccounts] = useState([]);
     const [distributionRules, setDistributionRules] = useState([]);
     const [incomingDeposits, setIncomingDeposits] = useState([]);
@@ -19,11 +20,10 @@ const FetchForHomepage = () => {
         usersState: [users, setUsers]
     }
 
-    // Base Fetch URL
     const BASE_URL = "https://transposit.onrender.com";
     const navigate = useNavigate()
 
-    // useEffects
+    // useEffect: Accounts
     useEffect(() => {
         async function fetchAccounts () {
             try {
@@ -42,6 +42,103 @@ const FetchForHomepage = () => {
             }
         }
         fetchAccounts();
+    }, [])
+
+    // useEffect: Distribution Rules
+    useEffect(() => {
+        async function fetchDistributionRules () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/distributionRules`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const distributionRulesData = await response.json();
+                console.log("Translated Distribution Rules Data:", distributionRulesData);
+                setDistributionRules(distributionRulesData);
+            } catch (error) {
+                console.log ("Error fetching Distribution Rules Data!")
+                console.log(error)
+            }
+        }
+        fetchDistributionRules();
+    }, [])
+
+    // useEffect: Incoming Deposits
+    useEffect(() => {
+        async function fetchIncomingDeposits () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/incomingDeposits`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const incomingDepositsData = await response.json();
+                console.log("Translated Incoming Deposits Data:", incomingDepositsData);
+                setIncomingDeposits(incomingDepositsData);
+            } catch (error) {
+                console.log ("Error fetching Incoming Deposits Data!")
+                console.log(error)
+            }
+        }
+        fetchIncomingDeposits();
+    }, [])
+
+    // useEffect: Transactions
+    useEffect(() => {
+        async function fetchTransactions () {
+            try {
+                const response = await fetch (`${BASE_URL}/api/transactions`, {
+                    headers: {
+                        'Content-Type' : 'application/json'
+                    }
+                })
+                console.log("Response", response)
+                const transactionsData = await response.json();
+                console.log("Translated Transactions Data:", transactionsData);
+                setTransactions(transactionsData);
+            } catch (error) {
+                console.log ("Error fetching Transactions Data!")
+                console.log(error)
+            }
+        }
+        fetchTransactions();
+    }, [])
+
+    // useEffect: Users
+    useEffect(() => {
+        async function fetchUsers () {
+            const token = localStorage.getItem("token");
+            if (!token) {
+                console.error("Authentication token is not available. Redirecting to login.");
+                navigate('/login');
+                return;
+            }
+    
+            try {
+                const response = await fetch (`${BASE_URL}/api/users`, {
+                    headers: {
+                        'Content-Type' : 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`); 
+                }
+    
+                console.log("Response", response)
+                const usersData = await response.json();
+                console.log("Translated Users Data:", usersData);
+                setUsers(usersData.user);
+            } catch (error) {
+                console.log ("Error fetching Users Data!")
+                console.log(error)
+            }
+        }
+        fetchUsers();
     }, [])
 
 // Return
